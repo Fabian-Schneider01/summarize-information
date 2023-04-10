b6a52516ae879814225221c8fe2d48db584eb2e5
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-04-10 19:59:40 +0200

    fix(settings.py): allowed hosts

--- summarize_information/summarize_information/settings.py
+++ summarize_information/summarize_information/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
