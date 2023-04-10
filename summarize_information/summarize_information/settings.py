697201a2debddfca24b734dd7ceab14d673ade94
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-04-10 21:14:23 +0200

    fix(ip): new config

--- summarize_information/summarize_information/settings.py
+++ summarize_information/summarize_information/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*', '20.199.102.204', 'summarize-information.francecentral.cloudapp.azure.com']
+ALLOWED_HOSTS = ['20.199.102.204']
 
 
 # Application definition
