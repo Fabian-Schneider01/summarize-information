98f70901e17178cb352e6039ba0dc400ed3e4c6f
Fabian-Schneider01 <xf.schneider01@gmail.com>
2023-04-10 22:13:59 +0200

    fix(settings): all hosts

--- summarize_information/summarize_information/settings.py
+++ summarize_information/summarize_information/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['20.199.102.204']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
